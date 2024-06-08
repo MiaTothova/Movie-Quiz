e66dc2c0b5f6f6752a262f6c2df6ebffe72537ee
MiaTothova <21tothova@gmail.com>
2024-06-08 07:58:46 +0000

    Add to startGame function: make answer options re-appear after start is clicked.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,17 @@
 
-const startButton = document.getElementById('start-btn'); 
-startButton.addEventListener('click', startGame); 
+const startButton = document.getElementById("start-btn"); 
+startButton.addEventListener("click", startGame); 
 
-const introHeading = document.getElementById('intro');
+const introHeading = document.getElementById("intro");
+
+const questionContainer = document.getElementById("answer-buttons"); 
 
 function startGame() {
     
-    startButton.classList.add('hide');     
-    introHeading.classList.add('hide');
+    startButton.classList.add("hide");     
+    introHeading.classList.add("hide");
+     
+    questionContainer.classList.remove("hide");
+    answerButtonsElement.classList.remove("hide");
 }
 
