483b871ae55b6ee045e659f45b8b09ad5ea9f8ee
MiaTothova <21tothova@gmail.com>
2024-06-08 07:43:58 +0000

    Add event listeners and startGame function to hide start buton and intro heading when clicked start

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,12 @@
-console.log("connected!")
+
+const startButton = document.getElementById('start-btn'); 
+startButton.addEventListener('click', startGame); 
+
+const introHeading = document.getElementById('intro');
+
+function startGame() {
+    
+    startButton.classList.add('hide');     
+    introHeading.classList.add('hide');
+}
+
