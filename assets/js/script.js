f281f2c766eed425b246d61e5c26db6d95b0cf88
MiaTothova <21tothova@gmail.com>
2024-06-10 10:53:35 +0000

    Add set next question function to display shuffled questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,36 @@
+const startButton = document.getElementById("start-btn"); 
+const questionContainerElement = document.getElementById("question-container");
+const questionElement = document.getElementById("question");
+const answerButtonsElement = document.getElementById("answer-buttons");
+
+let shuffledQuestions, currentQuestionIndex;
+
+startButton.addEventListener("click", startGame);
+
+
+function startGame() {
+    startButton.classList.add("hide");
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0
+
+    questionContainerElement.classList.remove("hide");
+
+    setNextQuestion();
+}
+
+function setNextQuestion() {
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
+
+}
+
+function showQuestion(question) {
+    questionElement.innerHTML = question.question
+}
+
+
+function selectAnswer() {
+
+}
 
 
 
@@ -10,11 +43,7 @@
 
 
 
-
-
-
-// const startButton = document.getElementById("start-btn"); 
-// startButton.addEventListener("click", startGame); 
+ 
 
 // const questionElement = document.getElementById('question')
 // const answerButtons = document.getElementById('answer-buttons')
