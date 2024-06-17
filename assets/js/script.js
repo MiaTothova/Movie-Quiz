b48313fa408c250e4c0c5f81ef56badc8f55e4da
Marketa Mia Tothova <21tothova@gmail.com>
2024-06-17 19:35:02 +0000

    Add more images and add them to questions

--- assets/js/script.js
+++ assets/js/script.js
@@ -231,94 +231,98 @@ const shortGameQuestions = [{
     ]
     
 },
-// {
-//     question: "Which 1994 film features the quote, 'Life is like a box of chocolates'?",
-//     answers: [{
-//             text: "The Shawshank Redemption",
-//             correct: false
-//         },
-//         {
-//             text: "Pulp Fiction ",
-//             correct: false
-//         },
-//         {
-//             text: "The Lion King ",
-//             correct: false
-//         },
-//         {
-//             text: "Forrest Gump",
-//             correct: true
-//         }
+{
+    question: "Which 1994 film features the quote, 'Life is like a box of chocolates'?",
+    image: "assets/images/Q4.webp",
+    answers: [{
+            text: "The Shawshank Redemption",
+            correct: false
+        },
+        {
+            text: "Pulp Fiction ",
+            correct: false
+        },
+        {
+            text: "The Lion King ",
+            correct: false
+        },
+        {
+            text: "Forrest Gump",
+            correct: true
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "What is the highest-grossing film of all time as of 2023?",
-//     answers: [{
-//             text: "Avengers: Endgame",
-//             correct: false
-//         },
-//         {
-//             text: "Avatar: The Way of Water",
-//             correct: true
-//         },
-//         {
-//             text: "Star Wars: The Force Awakens ",
-//             correct: false
-//         },
-//         {
-//             text: "Titanic",
-//             correct: false
-//         }
+},
+{
+    question: "What is the highest-grossing film of all time as of 2023?",
+    image: "assets/images/Q5.webp",
+    answers: [{
+            text: "Avengers: Endgame",
+            correct: false
+        },
+        {
+            text: "Avatar: The Way of Water",
+            correct: true
+        },
+        {
+            text: "Star Wars: The Force Awakens ",
+            correct: false
+        },
+        {
+            text: "Titanic",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "Who played the Joker in the 2008 film 'The Dark Knight'? ",
-//     answers: [{
-//             text: "Heath Ledger ",
-//             correct: true
-//         },
-//         {
-//             text: "Jack Nicholson ",
-//             correct: false
-//         },
-//         {
-//             text: "Joaquin Phoenix ",
-//             correct: false
-//         },
-//         {
-//             text: "Jared Leto ",
-//             correct: false
-//         }
+},
+{
+    question: "Who played the Joker in the 2008 film 'The Dark Knight'? ",
+    image: "assets/images/Q6.webp",
+    answers: [{
+            text: "Heath Ledger ",
+            correct: true
+        },
+        {
+            text: "Jack Nicholson ",
+            correct: false
+        },
+        {
+            text: "Joaquin Phoenix ",
+            correct: false
+        },
+        {
+            text: "Jared Leto ",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: " In 'Jurassic Park,' what type of dinosaur is the main antagonist?",
-//     answers: [{
-//             text: "Velociraptor ",
-//             correct: false
-//         },
-//         {
-//             text: "T. rex ",
-//             correct: true
-//         },
-//         {
-//             text: "Triceratops ",
-//             correct: false
-//         },
-//         {
-//             text: "Stegosaurus ",
-//             correct: false
-//         }
+},
+{
+    question: " In 'Jurassic Park,' what type of dinosaur is the main antagonist?",
+    image: "assets/images/Q7.webp",
+    answers: [{
+            text: "Velociraptor ",
+            correct: false
+        },
+        {
+            text: "T. rex ",
+            correct: true
+        },
+        {
+            text: "Triceratops ",
+            correct: false
+        },
+        {
+            text: "Stegosaurus ",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
+},
 
 ];
 
@@ -326,6 +330,7 @@ const shortGameQuestions = [{
 const longGameQuestions = [
 {
     question: "What is the name of the hobbit played by Elijah Wood in 'The Lord of the Rings' series? ",
+    image: "assets/images/Q8.webp",
     answers: [{
             text: "Bilbo Baggins ",
             correct: false
@@ -348,6 +353,7 @@ const longGameQuestions = [
 },
 {
     question: "Who directed the 1977 movie 'Star Wars' (later retitled 'Star Wars: Episode IV – A New Hope)? ",
+    image: "assets/images/Q9.webp",
     answers: [{
             text: "Steven Spielberg",
             correct: false
@@ -370,6 +376,7 @@ const longGameQuestions = [
 },
 {
     question: "What is the name of the school that Harry Potter attends?",
+    image: "assets/images/Q10.webp",
     answers: [{
             text: "Durmstrang Institute ",
             correct: false
@@ -392,6 +399,7 @@ const longGameQuestions = [
 },
 {
     question: "In which movie does Tom Hanks character say, “Houston, we have a problem”? ",
+    image: "assets/images/Q11.webp",
     answers: [{
             text: "Cast Away ",
             correct: false
@@ -412,204 +420,213 @@ const longGameQuestions = [
     ]
     
 },
-// {
-//     question: "What’s the name of the skyscraper in 'Die Hard'? ",
-//     answers: [{
-//             text: "Willis Tower ",
-//             correct: false
-//         },
-//         {
-//             text: "Empire State Building ",
-//             correct: false
-//         },
-//         {
-//             text: "Nakatomi Plaza ",
-//             correct: true
-//         },
-//         {
-//             text: "Stark Tower ",
-//             correct: false
-//         }
+{
+    question: "What’s the name of the skyscraper in 'Die Hard'? ",
+    image: "assets/images/Q12.webp",
+    answers: [{
+            text: "Willis Tower ",
+            correct: false
+        },
+        {
+            text: "Empire State Building ",
+            correct: false
+        },
+        {
+            text: "Nakatomi Plaza ",
+            correct: true
+        },
+        {
+            text: "Stark Tower ",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "Who is the actress that plays Katniss Everdeen in 'The Hunger Games' series? ",
-//     answers: [{
-//             text: "Emma Stone ",
-//             correct: false
-//         },
-//         {
-//             text: "Kristen Stewart ",
-//             correct: false
-//         },
-//         {
-//             text: "Shailene Woodley",
-//             correct: false
-//         },
-//         {
-//             text: "Jennifer Lawrence ",
-//             correct: true
-//         }
+},
+{
+    question: "Who is the actress that plays Katniss Everdeen in 'The Hunger Games' series? ",
+    image: "assets/images/Q13.webp",
+    answers: [{
+            text: "Emma Stone ",
+            correct: false
+        },
+        {
+            text: "Kristen Stewart ",
+            correct: false
+        },
+        {
+            text: "Shailene Woodley",
+            correct: false
+        },
+        {
+            text: "Jennifer Lawrence ",
+            correct: true
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: " What is the fictional African country where 'Black Panther' is set? ",
-//     answers: [{
-//             text: "Zamunda ",
-//             correct: false
-//         },
-//         {
-//             text: "Wakanda ",
-//             correct: true
-//         },
-//         {
-//             text: "Genovia ",
-//             correct: false
-//         },
-//         {
-//             text: "Latveria",
-//             correct: false
-//         }
+},
+{
+    question: " What is the fictional African country where 'Black Panther' is set? ",
+    image: "assets/images/Q14.webp",
+    answers: [{
+            text: "Zamunda ",
+            correct: false
+        },
+        {
+            text: "Wakanda ",
+            correct: true
+        },
+        {
+            text: "Genovia ",
+            correct: false
+        },
+        {
+            text: "Latveria",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "What animated film features the song “Let It Go”? ",
-//     answers: [{
-//             text: "Moana ",
-//             correct: false
-//         },
-//         {
-//             text: "Tangled",
-//             correct: false
-//         },
-//         {
-//             text: "Frozen",
-//             correct: true
-//         },
-//         {
-//             text: "Brave",
-//             correct: false
-//         }
+},
+{
+    question: "What animated film features the song “Let It Go”? ",
+    image: "assets/images/Q15.webp",
+    answers: [{
+            text: "Moana ",
+            correct: false
+        },
+        {
+            text: "Tangled",
+            correct: false
+        },
+        {
+            text: "Frozen",
+            correct: true
+        },
+        {
+            text: "Brave",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: " In 'Back to the Future,' what is the name of the scientist who invents the time machine?",
-//     answers: [{
-//             text: "Frankenstein",
-//             correct: false
-//         },
-//         {
-//             text: "Strange",
-//             correct: false
-//         },
-//         {
-//             text: "Jekyll",
-//             correct: false
-//         },
-//         {
-//             text: "Emmet Brown",
-//             correct: true
-//         }
+},
+{
+    question: " In 'Back to the Future,' what is the name of the scientist who invents the time machine?",
+    image: "assets/images/Q16.webp",
+    answers: [{
+            text: "Frankenstein",
+            correct: false
+        },
+        {
+            text: "Strange",
+            correct: false
+        },
+        {
+            text: "Jekyll",
+            correct: false
+        },
+        {
+            text: "Emmet Brown",
+            correct: true
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: " Which movie features the character Jack Sparrow? ",
-//     answers: [{
-//             text: "The Mummy ",
-//             correct: false
-//         },
-//         {
-//             text: "National Treasure ",
-//             correct: false
-//         },
-//         {
-//             text: "Indiana Jones ",
-//             correct: false
-//         },
-//         {
-//             text: "Pirates of the Caribbean ",
-//             correct: true
-//         }
+},
+{
+    question: " Which movie features the character Jack Sparrow? ",
+    image: "assets/images/Q17.webp",
+    answers: [{
+            text: "The Mummy ",
+            correct: false
+        },
+        {
+            text: "National Treasure ",
+            correct: false
+        },
+        {
+            text: "Indiana Jones ",
+            correct: false
+        },
+        {
+            text: "Pirates of the Caribbean ",
+            correct: true
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "In 'The Godfather,' what is the name of the character played by Marlon Brando? ",
-//     answers: [{
-//             text: "Michael Corleone ",
-//             correct: false
-//         },
-//         {
-//             text: "Sonny Corleone ",
-//             correct: false
-//         },
-//         {
-//             text: "Vito Corleone ",
-//             correct: true
-//         },
-//         {
-//             text: "Fredo Corleone ",
-//             correct: false
-//         }
+},
+{
+    question: "In 'The Godfather,' what is the name of the character played by Marlon Brando? ",
+    image: "assets/images/Q18.webp",
+    answers: [{
+            text: "Michael Corleone ",
+            correct: false
+        },
+        {
+            text: "Sonny Corleone ",
+            correct: false
+        },
+        {
+            text: "Vito Corleone ",
+            correct: true
+        },
+        {
+            text: "Fredo Corleone ",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: "Which film features the iconic line, “Here’s looking at you, kid”? ",
-//     answers: [{
-//             text: "Gone with the Wind ",
-//             correct: false
-//         },
-//         {
-//             text: "Citizen Kane ",
-//             correct: false
-//         },
-//         {
-//             text: "Casablanca ",
-//             correct: true
-//         },
-//         {
-//             text: "Breakfast at Tiffany's ",
-//             correct: true
-//         }
+},
+{
+    question: "Which film features the iconic line, “Here’s looking at you, kid”? ",
+    image: "assets/images/Q19.webp",
+    answers: [{
+            text: "Gone with the Wind ",
+            correct: false
+        },
+        {
+            text: "Citizen Kane ",
+            correct: false
+        },
+        {
+            text: "Casablanca ",
+            correct: true
+        },
+        {
+            text: "Breakfast at Tiffany's ",
+            correct: true
+        }
 
-//     ]
+    ]
     
-// },
-// {
-//     question: " In 'E.T. the Extra-Terrestrial,' what does E.T. use to phone home? ",
-//     answers: [{
-//             text: "Walkie-Talkie ",
-//             correct: false
-//         },
-//         {
-//             text: "Homemade communication device ",
-//             correct: true
-//         },
-//         {
-//             text: "Radio",
-//             correct: false
-//         },
-//         {
-//             text: "Cell Phone",
-//             correct: false
-//         }
+},
+{
+    question: " In 'E.T. the Extra-Terrestrial,' what does E.T. use to phone home? ",
+    image: "assets/images/Q20.webp",
+    answers: [{
+            text: "Walkie-Talkie ",
+            correct: false
+        },
+        {
+            text: "Homemade communication device ",
+            correct: true
+        },
+        {
+            text: "Radio",
+            correct: false
+        },
+        {
+            text: "Cell Phone",
+            correct: false
+        }
 
-//     ]
+    ]
     
-// },
+},
 
 
 ];
